3cdd8a42a3fd0a3cf92c775bc7c602de43e4f3cc
jacobhq <jacob.marshall.jem@gmail.com>
2021-02-11 12:19:20 +0000

    Stuff

---
+++ app.js
@@ -0,0 +1,11 @@
+TweenMax.defaultEase = Linear.easeOut;
+
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  navigation: true,
+  onLeave: (origin, destination, direction) => {
+    const section = destination.item;
+    const tl = new TimelineMax({ delay: 0.5 });
+  }
+});
